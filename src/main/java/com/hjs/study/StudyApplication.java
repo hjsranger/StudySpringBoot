af1ce417f94b7106da1c188598f1f8434acc334b
jianshuang_huang <hjsranger@163.com>
2018-06-24 09:43:29 +0800

    init

--- src/main/java/com/hjs/study/StudyApplication.java
+++ src/main/java/com/hjs/study/StudyApplication.java
@@ -1,9 +1,11 @@
 package com.hjs.study;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hjs.mapper")//将项目中对应的mapper类的路径加进来就可以了
 public class StudyApplication {
 
 	public static void main(String[] args) {
